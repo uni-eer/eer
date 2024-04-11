1b9990798d0b8ffee64dbe7bfbc52474f37ee40d
ajstrongdev <ajstrong.dev@gmail.com>
2024-04-11 15:13:37 +0100

    Enh: Make code pretty

--- admin/users.php
+++ admin/users.php
@@ -4,7 +4,7 @@ if (!isset($_SESSION['role'])) {
   header("location: ../login.php");
 }
 $user_id = $_SESSION['user_id'];
-if(intval($_SESSION['role']) !== 1){
+if (intval($_SESSION['role']) !== 1) {
   header("location: ../logout.php");
 }
 $showError = false;
@@ -15,82 +15,86 @@ $data = mysqli_query($conn, "SELECT * FROM `users` order by id desc");
 ?>
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Manage Users</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://cdn.datatables.net/2.0.2/css/dataTables.dataTables.min.css">
-    <script src="https://cdn.datatables.net/2.0.2/js/dataTables.min.js"></script>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Manage Users</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://cdn.datatables.net/2.0.2/css/dataTables.dataTables.min.css">
+  <script src="https://cdn.datatables.net/2.0.2/js/dataTables.min.js"></script>
 </head>
+
 <body>
-    <!-- Navbar -->
-    <?php include("header.php"); ?>
-   <div class="container mx-auto mt-5">
+  <!-- Navbar -->
+  <?php include("header.php"); ?>
+  <div class="container mx-auto mt-5">
     <div class="d-flex justify-content-between">
-<div><h2>Manage Users</h2></div>
+      <div>
+        <h2>Manage Users</h2>
+      </div>
     </div>
-    <?php if($showAlert){ ?>
+    <?php if ($showAlert) { ?>
       <div class="alert alert-success" role="alert">
-  <?=$showAlert?>
-</div>
-    <?php } ?> 
-    <?php if($showError){ ?>
-      <div class="alert alert-danger" role="alert">
-      <?=$showError?>
-</div>
+        <?= $showAlert ?>
+      </div>
     <?php } ?>
-   <table class="table table-bordered table-striped table-hover" id="myTable">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First Name</th>
-      <th scope="col">Last Name</th>
-      <th scope="col">Username</th>
-      <th scope="col">Email</th>
-      <th scope="col">Role</th>
-      <th scope="col">Regsitered At</th>
-      <th scope="col">View</th>
-      <th scope="col">Edit</th>
-      <th scope="col">Delete</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php if(mysqli_num_rows($data)>0){
-      $sno = 0;
-      while($fetch_rows = mysqli_fetch_assoc($data)){
-        $sno++;
-        $role_get = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM `roles` WHERE `id`=$fetch_rows[role_id]"));
-?>
-    <tr>
-      <th scope="row"><?=$sno?></th>
-      <td><?=$fetch_rows['fname']?></td>
-      <td><?=$fetch_rows['lname']?></td>
-      <td><?=$fetch_rows['username']?></td>
-      <td><?=$fetch_rows['email']?></td>
-      <td><?=$role_get['name']?></td>
-      <td><?=date('d M, Y h:i A', strtotime($fetch_rows['created_at']))?></td>
-      <td><a href="view_user.php?id=<?=$fetch_rows['id']?>" class="btn btn-dark">View</a></td>
-      <td><a href="edit_user.php?id=<?=$fetch_rows['id']?>" class="btn btn-success">Edit</a></td>
-      <td><a href="delete_user.php?id=<?=$fetch_rows['id']?>" class="btn btn-danger">Delete</a></td>
-    </tr>
-<?php
-      }
-    } ?>
+    <?php if ($showError) { ?>
+      <div class="alert alert-danger" role="alert">
+        <?= $showError ?>
+      </div>
+    <?php } ?>
+    <table class="table table-bordered table-striped table-hover" id="myTable">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">First Name</th>
+          <th scope="col">Last Name</th>
+          <th scope="col">Username</th>
+          <th scope="col">Email</th>
+          <th scope="col">Role</th>
+          <th scope="col">Regsitered At</th>
+          <th scope="col">View</th>
+          <th scope="col">Edit</th>
+          <th scope="col">Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php if (mysqli_num_rows($data) > 0) {
+          $sno = 0;
+          while ($fetch_rows = mysqli_fetch_assoc($data)) {
+            $sno++;
+            $role_get = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM `roles` WHERE `id`=$fetch_rows[role_id]"));
+        ?>
+            <tr>
+              <th scope="row"><?= $sno ?></th>
+              <td><?= $fetch_rows['fname'] ?></td>
+              <td><?= $fetch_rows['lname'] ?></td>
+              <td><?= $fetch_rows['username'] ?></td>
+              <td><?= $fetch_rows['email'] ?></td>
+              <td><?= $role_get['name'] ?></td>
+              <td><?= date('d M, Y h:i A', strtotime($fetch_rows['created_at'])) ?></td>
+              <td><a href="view_user.php?id=<?= $fetch_rows['id'] ?>" class="btn btn-dark">View</a></td>
+              <td><a href="edit_user.php?id=<?= $fetch_rows['id'] ?>" class="btn btn-success">Edit</a></td>
+              <td><a href="delete_user.php?id=<?= $fetch_rows['id'] ?>" class="btn btn-danger">Delete</a></td>
+            </tr>
+        <?php
+          }
+        } ?>
 
-  </tbody>
-</table>
-   </div>
+      </tbody>
+    </table>
+  </div>
 
 
 
 
-   <script>
-    $(document).ready( function () {
-    $('#myTable').DataTable({
-        order:false,
+  <script>
+    $(document).ready(function() {
+      $('#myTable').DataTable({
+        order: false,
+      });
     });
-} );
-   </script>
-    <?php include("footer.php"); ?>
+  </script>
+  <?php include("footer.php"); ?>
--- admin/view_user.php
+++ admin/view_user.php
@@ -1,20 +1,20 @@
-<?php 
+<?php
 include "../php/db.php";
 
 // Authentication check
 if (!isset($_SESSION['role'])) {
-    header("location: ../login.php");
+  header("location: ../login.php");
+}
+if (intval($_SESSION['role']) !== 1) {
+  header("location: ../logout.php");
+}
+if (!isset($_GET['id'])) {
+  if (isset($_SERVER['HTTP_REFERER'])) { // Get prevoius location
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
   }
-  if(intval($_SESSION['role']) !== 1){
-    header("location: ../logout.php");
-  }
-  if(!isset($_GET['id'])){
-    if(isset($_SERVER['HTTP_REFERER'])) { // Get prevoius location
-        header('Location: ' . $_SERVER['HTTP_REFERER']);
-    }
-exit();
-  }
-  $id = $_GET['id'];
+  exit();
+}
+$id = $_GET['id'];
 $user_id = $_SESSION['user_id'];
 $showError = false;
 $showAlert = false;
@@ -22,78 +22,82 @@ $showAlert = false;
 
 
 $user_data = mysqli_query($conn, "SELECT * FROM `users` WHERE `id`=$id");
-if(mysqli_num_rows($user_data)===0){
-    if(isset($_SERVER['HTTP_REFERER'])) { // Get prevoius location
-        header('Location: ' . $_SERVER['HTTP_REFERER']);
-    }
-exit();
+if (mysqli_num_rows($user_data) === 0) {
+  if (isset($_SERVER['HTTP_REFERER'])) { // Get prevoius location
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
+  }
+  exit();
 }
 $data = mysqli_fetch_assoc($user_data);
 $role_get = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM `roles` WHERE `id`=$data[role_id]"));
 ?>
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>View User</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://cdn.datatables.net/2.0.2/css/dataTables.dataTables.min.css">
-    <script src="https://cdn.datatables.net/2.0.2/js/dataTables.min.js"></script>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>View User</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://cdn.datatables.net/2.0.2/css/dataTables.dataTables.min.css">
+  <script src="https://cdn.datatables.net/2.0.2/js/dataTables.min.js"></script>
 </head>
+
 <body>
-    <!-- Navbar -->
-    <?php include("header.php"); ?>
-   <div style="max-width: 500px" class=" mx-auto mt-5">
+  <!-- Navbar -->
+  <?php include("header.php"); ?>
+  <div style="max-width: 500px" class=" mx-auto mt-5">
     <div class="d-flex justify-content-between">
-<div><h2>View User</h2></div>
+      <div>
+        <h2>View User</h2>
+      </div>
     </div>
-    <?php if($showAlert){ ?>
+    <?php if ($showAlert) { ?>
       <div class="alert alert-success" role="alert">
-  <?=$showAlert?>
-</div>
-    <?php } ?> 
-    <?php if($showError){ ?>
+        <?= $showAlert ?>
+      </div>
+    <?php } ?>
+    <?php if ($showError) { ?>
       <div class="alert alert-danger" role="alert">
-      <?=$showError?>
-</div>
+        <?= $showError ?>
+      </div>
     <?php } ?>
     <form method="post">
-  <div class="mb-3">
-    <label for="fname" class="form-label">First name</label>
-    <input type="text" name="fname" class="form-control" id="fname" value="<?=$data['fname']?>" readonly disabled>
+      <div class="mb-3">
+        <label for="fname" class="form-label">First name</label>
+        <input type="text" name="fname" class="form-control" id="fname" value="<?= $data['fname'] ?>" readonly disabled>
+      </div>
+      <div class="mb-3">
+        <label for="lname" class="form-label">Last name</label>
+        <input type="text" name="lname" class="form-control" id="lname" value="<?= $data['lname'] ?>" readonly disabled>
+      </div>
+      <div class="mb-3">
+        <label for="username" class="form-label">User name</label>
+        <input type="text" name="username" class="form-control" id="username" value="<?= $data['username'] ?>" readonly disabled>
+      </div>
+      <input type="hidden" name="id" value="<?= $data['id'] ?>">
+      <div class="mb-3">
+        <label for="phone" class="form-label">Phone Number</label>
+        <input type="text" name="phone" class="form-control" id="phone" value="<?= $data['phone'] ?>" readonly disabled>
+      </div>
+      <div class="mb-3">
+        <label for="address" class="form-label">Address</label>
+        <input type="text" name="address" class="form-control" id="address" value="<?= $data['address'] ?>" readonly disabled>
+      </div>
+      <div class="mb-3">
+        <label for="email" class="form-label">Email</label>
+        <input type="text" name="email" class="form-control" id="email" value="<?= $data['email'] ?>" readonly disabled>
+      </div>
+      <div class="mb-3">
+        <label for="role" class="form-label">Role</label>
+        <input type="text" name="role" class="form-control" id="role" value="<?= $role_get['name'] ?>" readonly disabled>
+      </div>
+      <div class="mb-3">
+        <label for="registered_at" class="form-label">Registered At</label>
+        <input type="text" name="registered_at" class="form-control" id="registered_at" value="<?= date('d M, Y h:i A', strtotime($data['created_at'])) ?>" readonly disabled>
+      </div>
+    </form>
   </div>
-  <div class="mb-3">
-    <label for="lname" class="form-label">Last name</label>
-    <input type="text" name="lname" class="form-control" id="lname" value="<?=$data['lname']?>" readonly disabled>
-  </div>
-  <div class="mb-3">
-    <label for="username" class="form-label">User name</label>
-    <input type="text" name="username" class="form-control" id="username" value="<?=$data['username']?>" readonly disabled>
-  </div>
-  <input type="hidden" name="id" value="<?=$data['id']?>">
-  <div class="mb-3">
-    <label for="phone" class="form-label">Phone Number</label>
-    <input type="text" name="phone" class="form-control" id="phone" value="<?=$data['phone']?>" readonly disabled>
-  </div>
-  <div class="mb-3">
-    <label for="address" class="form-label">Address</label>
-    <input type="text" name="address" class="form-control" id="address" value="<?=$data['address']?>" readonly disabled>
-  </div>
-  <div class="mb-3">
-    <label for="email" class="form-label">Email</label>
-    <input type="text" name="email" class="form-control" id="email" value="<?=$data['email']?>" readonly disabled>
-  </div>
-  <div class="mb-3">
-    <label for="role" class="form-label">Role</label>
-    <input type="text" name="role" class="form-control" id="role" value="<?=$role_get['name']?>" readonly disabled>
-  </div>
-  <div class="mb-3">
-    <label for="registered_at" class="form-label">Registered At</label>
-    <input type="text" name="registered_at" class="form-control" id="registered_at" value="<?=date('d M, Y h:i A', strtotime($data['created_at']))?>" readonly disabled>
-  </div>
-</form>
-   </div>
 
-   <?php include("footer.php"); ?>
+  <?php include("footer.php"); ?>
